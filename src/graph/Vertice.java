526a565bab2e72fc9ab332315ba77de9704c4e90
Camille Briand <camille.briand@efrei.net>
2019-02-15 11:44:04 +0100

    Add base constructor (with id) for graph.Vertice class

--- src/graph/Vertice.java
+++ src/graph/Vertice.java
@@ -1,4 +1,18 @@
 package graph;
 
 public class Vertice {
+
+	/**
+	 * Id of the vertice
+	 */
+	private int id;
+
+
+	/**
+	 * Create a new {@link Vertice} instance with passed id
+	 * @param id ID of the vertice to create
+	 */
+	public Vertice (int id) {
+		this.id = id;
+	}
 }
