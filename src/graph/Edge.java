f58f70cf7167189fad17e2db596e71e630f90f9d
Camille Briand <camille.briand@efrei.net>
2019-02-20 13:57:03 +0100

    Add properties to class graph.Edge

--- src/graph/Edge.java
+++ src/graph/Edge.java
@@ -1,4 +1,25 @@
 package graph;
 
 public class Edge {
+
+	/**
+	 * Default weight for the {@link Edge}
+	 */
+	public final static int DEFAULT_WEIGHT = 1;
+
+	/**
+	 * The {@link Vertice} from which the {@link Edge} starts
+	 */
+	private Vertice startVertice;
+
+	/**
+	 * The {@link Vertice} to which the {@link Edge} goes
+	 */
+	private Vertice endVertice;
+
+
+	/**
+	 * The {@code weight} of the {@link Edge} (cost)
+	 */
+	private int weight = DEFAULT_WEIGHT;
 }
