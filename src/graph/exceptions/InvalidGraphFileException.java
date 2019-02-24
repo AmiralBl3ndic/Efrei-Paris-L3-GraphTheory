6615dc6e5db58a1a207625ccf50ce9107ac3a446
AmiralBl3ndic <camille.briand@efrei.net>
2019-02-24 10:37:41 +0100

    Add the graph.exceptions.InvalidGraphFileException exception

---
+++ src/graph/exceptions/InvalidGraphFileException.java
@@ -0,0 +1,16 @@
+package graph.exceptions;
+
+/**
+ * Exception to throw when a graph file does not contain the right data, or has a structure problem
+ */
+public class InvalidGraphFileException extends IllegalFileException {
+	private static final String DEFAULT_MESSAGE = "The passed Graph file contains a structural error: impossible to build a graph with this file";
+
+	public InvalidGraphFileException () {
+		super(DEFAULT_MESSAGE);
+	}
+
+	public InvalidGraphFileException (String message) {
+		super(message);
+	}
+}
