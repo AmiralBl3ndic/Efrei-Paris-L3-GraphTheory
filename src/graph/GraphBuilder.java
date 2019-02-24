3feb4fe54e56d8daf7e966997900b87278bacd8f
AmiralBl3ndic <camille.briand@efrei.net>
2019-02-24 10:58:52 +0100

    Add the GraphBuilder class

---
+++ src/graph/GraphBuilder.java
@@ -0,0 +1,14 @@
+package graph;
+
+
+/**
+ * Class that holds some static methods that are useful for building {@link Graph}s.
+ * Use this class for all static methods that can be used for building a new {@link Graph} so that the code in the original {@link Graph} class stays clean.
+ */
+final class GraphBuilder {
+
+	private GraphBuilder () {}  // Private constructor so that the class can not be instantiated
+
+
+
+}
