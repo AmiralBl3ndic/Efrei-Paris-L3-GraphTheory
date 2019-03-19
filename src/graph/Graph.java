d2ecc4b16a36e5f9f061ae556c54e22e067bbcc8
Camille Briand <camille.briand@efrei.net>
2019-03-19 08:00:28 +0100

    Remove the new useless GraphBuilder class

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -45,7 +45,7 @@ public final class Graph {
 
 
 		// Attempt to read the first line (number of vertices in the graph)
-		this.numberVertices = GraphBuilder.readNumberVertices(sc);
+		this.numberVertices = readNumberVertices(sc);
 
 		this.vertices = new ArrayList<>();
 		for (int i = 0; i < this.numberVertices; i++) {
@@ -129,6 +129,33 @@ public final class Graph {
 			}
 
 			System.out.print("\n");
+		}
+	}
+
+
+	/**
+	 * Read the first line of the passed file, check the value and act consequently
+	 * @param sc {@link Scanner} on the file to be read
+	 * @return Number of vertices in the graph according to the file
+	 * @throws InvalidGraphFileException If the file does not have the right format, structure or if data are not realistic (negative number of vertices)
+	 */
+	private static int readNumberVertices (Scanner sc) throws InvalidGraphFileException {
+		final int MINIMUM_NUMBER_OF_VERTICES = 1;
+
+		if (sc.hasNextInt()) {  // Check if able to read an integer from the scanner
+			int numberOfVertices = sc.nextInt();
+
+			// Check read value
+			if (numberOfVertices < MINIMUM_NUMBER_OF_VERTICES) {
+				throw new InvalidGraphFileException("A graph must contain at least " + MINIMUM_NUMBER_OF_VERTICES + " vertices, but this file specifies " + numberOfVertices);
+			}
+
+			sc.nextLine();  // Escaping the end line character
+
+			return numberOfVertices;  // Now safe to return
+
+		} else {  // Throw an InvalidGraphFileException if not able to read
+			throw new InvalidGraphFileException("Cannot read number of vertices from the passed file");
 		}
 	}
 }
