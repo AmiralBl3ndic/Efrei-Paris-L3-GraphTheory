e31988ffbc55add5992dd64fe225950eb13dbe59
Camille Briand <camille.briand@efrei.net>
2019-05-03 17:24:54 +0200

    Add the NotLinkedException to custom exceptions

---
+++ src/graph/exceptions/NotLinkedException.java
@@ -0,0 +1,17 @@
+package graph.exceptions;
+
+
+/**
+ * Exception to be thrown when a vertex is not linked to another (but should be for the operation to complete)
+ */
+public class NotLinkedException extends Exception {
+	final static String DEFAULT_MESSAGE = "The asked vertex is not linked";
+
+	public NotLinkedException() {
+		super(DEFAULT_MESSAGE);
+	}
+
+	public NotLinkedException(String message) {
+		super(message);
+	}
+}
