d7b8f067a6ac16f5be93c6ff3a075a493b20c5c3
Camille Briand <camille.briand@efrei.net>
2019-05-05 23:08:17 +0200

    Last commit, now trying to redo Dijkstra entirely by our own

--- src/graph/Dijkstra.java
+++ src/graph/Dijkstra.java
@@ -4,137 +4,135 @@ import java.util.*;
 
 public class Dijkstra {
 
-    private Graph g;
-    private ArrayList<Vertex> v;
-    private ArrayList<Integer> Distance;
-    private int DistanceFromS;
-    private ArrayList<Edge> currentVerticeEdges;
-    
-    Dijkstra() {
+	private Graph g;
+	private ArrayList<Vertex> v;
+	private ArrayList<Integer> Distance;
+	private int DistanceFromS;
+	private ArrayList<Edge> currentVerticeEdges;
 
-    }
+	Dijkstra() {
+
+	}
 
 
 	/**
 	 * Constructor
+	 *
 	 * @param g
 	 */
 	Dijkstra(Graph g) {
-        this.g = g;
-        this.DistanceFromS = 0;
+		this.g = g;
+		this.DistanceFromS = 0;
 
-        if (g.hasNegativeEdge())
-            System.out.println("We can't compute the Dijkstra's algorithm if there is negative edge !");
-        else {
-            int sizeG = g.getNbVertices();
-            boolean[] vVisited = new boolean[sizeG];
-            Arrays.fill(vVisited, false);
-            computeShortestDistances(vVisited);
-        }
-    }
-
-
-    public int DistanceFromSource(ArrayList<Edge> inEdges, int choice) {
-        if (choice == -1) { //Initialization
-            for (Edge e1 : inEdges) {
-                Vertex sV = e1.getStartVertex();
-                int weightFromId = sV.getId();
-                if (sV.getId() == 0) {
-                    this.DistanceFromS += weightFromId;  // x + 0 = x
-                    this.Distance.set(0, this.DistanceFromS);
-                    break;
-                }
-            }
-        }else {
-            Edge e2 = inEdges.get(choice);
-            Vertex vertexE = e2.getStartVertex();
-            int weightFromVE = vertexE.getId();
-            if (vertexE.getId() == choice) {
-                this.DistanceFromS += weightFromVE;
-                this.Distance.set(choice, this.DistanceFromS);
-            }
-        }
-        return DistanceFromS;
-    }
+		if (g.hasNegativeEdge())
+			System.out.println("We can't compute the Dijkstra's algorithm if there is negative edge !");
+		else {
+			int sizeG = g.getNbVertices();
+			boolean[] vVisited = new boolean[sizeG];
+			Arrays.fill(vVisited, false);
+			computeShortestDistances(vVisited);
+		}
+	}
 
 
-    public void computeShortestDistances(boolean[] vVisted) {
-        int nextVertex = 0;
-        v = g.getVertices;
-        Vertex v0 = v.get(0);
-        ArrayList<Edge> inEdges = v[0].getInEdges;
+	public int DistanceFromSource(ArrayList<Edge> inEdges, int choice) {
+		if (choice == -1) { //Initialization
+			for (Edge e1 : inEdges) {
+				Vertex sV = e1.getStartVertex();
+				int weightFromId = sV.getId();
+				if (sV.getId() == 0) {
+					this.DistanceFromS += weightFromId;  // x + 0 = x
+					this.Distance.set(0, this.DistanceFromS);
+					break;
+				}
+			}
+		} else {
+			Edge e2 = inEdges.get(choice);
+			Vertex vertexE = e2.getStartVertex();
+			int weightFromVE = vertexE.getId();
+			if (vertexE.getId() == choice) {
+				this.DistanceFromS += weightFromVE;
+				this.Distance.set(choice, this.DistanceFromS);
+			}
+		}
+		return DistanceFromS;
+	}
 
-        DistanceFromSource(inEdges, -1);
 
-        // we visit every  vetice
-        for (int i = 0 ; i < v.size() ; i++) {
+	public void computeShortestDistances(boolean[] vVisited) {
+		int nextVertex = 0;
+		v = g.getVertices();
+		ArrayList<Edge> inEdges = v.get(0).getInEdges();
 
-            currentVerticeEdges = v.get(nextVertex).getEdges();
+		DistanceFromSource(inEdges, -1);  // Initialization
 
-            for (int joinedEdge = 0; joinedEdge < currentVerticeEdges.size(); joinedEdge++) {
-                Edge eNow = currentVerticeEdges.get(joinedEdge);
-                Edge eNext = currentVerticeEdges.get(nextVertex);
-                Vertice verticeEnStart = eNow.getStartVertex();
-                Vertice verticeEnEnd = eNow.getEndVertex();
+		// we visit every vertices
+		for (int i = 0; i < v.size(); i++) {
 
-                if (verticeEnStart.getId == nextVertex) {
-                    int neighbourIndex = verticeEnStart.getId;
-                } else if (verticeEnStart.getId == nextVertex) {
-                    int neighbourIndex = verticeEnStart.getId;
-                }
+			currentVerticeEdges = v.get(nextVertex).getEdges();
 
-                // only if not visited
-                if ( !(vVisted[neighbourIndex]) ) {
-                    int tentative = DistanceFromSource(currentVerticeEdges, nextVertex) + eNext.getLength();
+			for (int joinedEdge = 0; joinedEdge < currentVerticeEdges.size(); joinedEdge++) {
+				Edge eNow = currentVerticeEdges.get(joinedEdge);
+				Edge eNext = currentVerticeEdges.get(nextVertex);
+				Vertex verticeEnStart = eNow.getStartVertex();
+				Vertex verticeEnEnd = eNow.getEndVertex();
 
-                    if (tentative < DistanceFromSource(currentVerticeEdges, nextVertex)) {
-                        Distance[neighbourIndex] = tentative;
-                    }
-                }
-            }
+				int neighbourIndex;
+				if (verticeEnStart.getId() == nextVertex) {
+					neighbourIndex = nextVertex;
+				}
 
-            // all neighbours checked so Vertice visited
-            vVisted[nextVertex] = true;
+				// only if not visited
+				if (!(vVisited[neighbourIndex])) {
+					int tentative = DistanceFromSource(currentVerticeEdges, nextVertex) + eNext.getWeight();
 
-            // next Vertice must be with shortest distance
-            nextVertex = getVerticesShortestDistanced();
+					if (tentative < DistanceFromSource(currentVerticeEdges, nextVertex)) {
+						this.Distance.set(neighbourIndex, tentative);
+					}
+				}
+			}
 
-        }
-    }
+			// all neighbours checked so Vertice visited
+			vVisited[nextVertex] = true;
 
-    private int getVerticesShortestDistanced() {
-        int storedVerticeIndex = 0;
-        int storedDist = Integer.MAX_VALUE;
+			// next Vertice must be with shortest distance
+			nextVertex = getVerticesShortestDistanced();
+		}
+	}
 
-        ArrayList<Vertex> vSucc =  v.getSuccessors();
+	private int getVerticesShortestDistanced() {
+		int storedVerticeIndex = 0;
+		int storedDist = Integer.MAX_VALUE;
 
-        for (int i = 0 ; i < vSucc.size() ; i++) {
-            Vertex currentSucc = vSucc.get(i);
-            ArrayList<Edge> edgeCurrSucc = currentSucc.getEdges();
+		ArrayList<Vertex> vSucc = v.getSuccessors();
 
-            for (int joinedEdge = 0; joinedEdge < currentVerticeEdges.size(); joinedEdge++) {
-                Edge eNow = edgeCurrSucc.get(joinedEdge);
-                Edge eNext = edgeCurrSucc.get(nextVertex);
-                Vertice verticeEnStart = eNow.getStartVertex();
-                Vertice verticeEnEnd = eNow.getEndVertex();
+		for (int i = 0; i < vSucc.size(); i++) {
+			Vertex currentSucc = vSucc.get(i);
+			ArrayList<Edge> edgeCurrSucc = currentSucc.getEdges();
 
-                int neighbourIndex;
-                if (verticeEnStart.getId == nextVertex || verticeStart.getId() == nextVertex) {
-                    neighbourIndex = verticeEnStart.getId;
-                } else if (verticeEnStart.getId == nextVertex) {
-                    int neighbourIndex = verticeEnStart.getId;
-                }
+			for (int joinedEdge = 0; joinedEdge < currentVerticeEdges.size(); joinedEdge++) {
+				Edge eNow = edgeCurrSucc.get(joinedEdge);
+				Edge eNext = edgeCurrSucc.get(nextVertex);
+				Vertice verticeEnStart = eNow.getStartVertex();
+				Vertice verticeEnEnd = eNow.getEndVertex();
 
-                int currentDist = DistanceFromSource(edgeCurrSucc, i);
+				int neighbourIndex;
+				if (verticeEnStart.getId == nextVertex || verticeStart.getId() == nextVertex) {
+					neighbourIndex = verticeEnStart.getId;
+				} else if (verticeEnStart.getId == nextVertex) {
+					int neighbourIndex = verticeEnStart.getId;
+				}
 
-                if ( (!vVisted[neighbourIndex]) && (currentDist < storedDist) ) {
-                    storedDist = currentDist;
-                    storedVerticeIndex = i;
-                }
-            }
-        }
+				int currentDist = DistanceFromSource(edgeCurrSucc, i);
 
-        return storedVerticeIndex;
-    }
+				if ((!vVisted[neighbourIndex]) && (currentDist < storedDist)) {
+					storedDist = currentDist;
+					storedVerticeIndex = i;
+				}
+			}
+		}
+
+		return storedVerticeIndex;
+	}
 
 }
