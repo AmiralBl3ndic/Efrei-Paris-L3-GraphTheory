d76952f152166103447c83f991855312508b3e44
Jeremi Friggit <jeremifk@gmail.com>
2019-05-06 01:27:17 +0200

    Finish up on TestClass to have it do what is asked

--- src/tests/TestClass.java
+++ src/tests/TestClass.java
@@ -4,21 +4,60 @@ import graph.*;
 import graph.exceptions.InvalidGraphFileException;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class TestClass {
 
 	public static void main(String[] args) {
-		try {
-			Graph myGraph = new Graph("test-graphs/L3Int-4-2.txt");
 
-			System.out.println(myGraph);
+		Scanner sc = new Scanner(System.in);
+		String filename;
+		String keepGoing;
+		do {
 
-			myGraph.applyBellmanFord(myGraph.getVertex(0));
+			filename = "test-graphs/L3Int-4-";
+			System.out.println("Enter the number of the graph file you wish to analyse :");
+			filename += sc.next();
+			filename += ".txt";
 
+			try {
+				Graph myGraph = new Graph(filename);
 
-		} catch (FileNotFoundException | InvalidGraphFileException e) {
-			e.printStackTrace();
-		}
+				System.out.println();
+				System.out.println("=== VALUE MATRIX ===");
+				System.out.println();
+				System.out.println(myGraph.toString());
+				System.out.println();
+				myGraph.displayAdjacencyMatrix();
+				System.out.println();
+				System.out.println("Enter the start vertex for the shortest paths calculation :");
+				int vertexId = sc.nextInt();
+
+				if (myGraph.hasNegativeEdge()) {
+					System.out.println();
+					System.out.println("The graph has a negative edge, applying Bellman's algorithm.");
+					System.out.println();
+					myGraph.applyBellmanFord(vertexId);
+				} else {
+					int algorithmChoice;
+					do {
+						System.out.println();
+						System.out.println("Please choose 1 for Dijkstra algorithm and 2 for Bellman algorithm :");
+						algorithmChoice = sc.nextInt();
+					} while (algorithmChoice != 1 && algorithmChoice != 2);
+					if (algorithmChoice == 1) {
+						myGraph.applyDijkstra(vertexId);
+					} else {
+						myGraph.applyBellmanFord(vertexId);
+					}
+				}
+			} catch (FileNotFoundException | InvalidGraphFileException e) {
+				e.printStackTrace();
+			}
+			System.out.println();
+			System.out.println("Please enter 0 to quit and anything else to choose a new file :");
+			keepGoing = sc.next();
+		} while (!keepGoing.equals("0"));
 	}
 
 }
